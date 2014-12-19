c97ca6d00046472ae3ba4405129090e540b09dee
cardeol <cardeol@gmail.com>
2014-12-19 11:55:36 +0000

    Adding Slim response

--- index.php
+++ index.php
@@ -3,12 +3,23 @@
 include("config.php");
 
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'debug' => true
+));
 
-// handle GET requests for /articles
+
 $app->get('/', function () use ($app) {  
-  	
+  	$response = array("test" =>"testing data");
+  	echoResponse(200,$response);
 });
+
+
+function echoResponse($status_code, $response)  {
+	$app = \Slim\Slim::getInstance();
+    $app->status($status_code);
+    $app->contentType('application/json'); 
+    echo json_encode($response);
+}
 
 
 $app->run();
--- tests/luasTest.php
+++ tests/luasTest.php
@@ -11,27 +11,12 @@ class XtTest extends PHPUnit_Framework_TestCase
  	public function testValidReturnData()
     {
 
-      	$luas = new LuasApi();
-		
-		$forecast = $luas->getForecast("bus", array("format" => "json", "return" => "true"));
+      	$luas = new LuasApi();		
+		$forecast = $luas->getForecast("bus", array("format" => "json");
 		$j = json_decode($forecast);
-		$this->assertTrue(is_array($j));
-		print_r($j);
+		$this->assertTrue(is_array($j));		
     }
-/*
-    public function testPushAndPop()
-    {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
-    */
 }
 ?>
 
